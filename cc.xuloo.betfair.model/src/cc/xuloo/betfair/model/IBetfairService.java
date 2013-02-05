0623cc8d480241ea95cd31d5a5e9a6c0b86a1e97
Trevor Burton <worldofpaper@googlemail.com>
2013-02-05 17:22:26 +0000

    initial import

---
+++ cc.xuloo.betfair.model/src/cc/xuloo/betfair/model/IBetfairService.java
@@ -0,0 +1,6 @@
+package cc.xuloo.betfair.model;
+
+public interface IBetfairService {
+
+	boolean login(String username, String password, int id);
+}
---
+++ cc.xuloo.betfair.service/src/cc/xuloo/betfair/service/BetfairService.java
@@ -0,0 +1,13 @@
+package cc.xuloo.betfair.service;
+
+import cc.xuloo.betfair.model.IBetfairService;
+
+public class BetfairService implements IBetfairService {
+
+	@Override
+	public boolean login(String username, String password, int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
