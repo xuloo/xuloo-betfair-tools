ae4cf53a75f5066b17843eb8abb168e8f1aac2d1
Trevor Burton <worldofpaper@googlemail.com>
2013-02-08 13:47:09 +0000

    added market data wrapper for flagging matched status

--- cc.xuloo.betfair.model/src/cc/xuloo/betfair/model/MarketDataWrapper.java
+++ cc.xuloo.betfair.model/src/cc/xuloo/betfair/model/MarketDataWrapper.java
@@ -4,7 +4,9 @@ public class MarketDataWrapper {
 
 	private MarketData marketData;
 	
-	private boolean matched;
+	private boolean matched = false;
+	
+	private boolean canMatch = true;
 	
 	public MarketDataWrapper(MarketData marketData) {
 		this.marketData = marketData;
@@ -20,5 +22,13 @@ public class MarketDataWrapper {
 	
 	public void setMatched(boolean matched) {
 		this.matched = matched;
+	}
+	
+	public void setBeMatched(boolean canMatch) {
+		this.canMatch = canMatch;
+	}
+	
+	public boolean canBeMatched() {
+		return canMatch;
 	}
 }
